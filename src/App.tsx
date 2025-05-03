e39a30e9cf92c4e59b520d72da445eee134fb09c
shinsukekikuchi <shinsukekikuchi111@gmail.com>
2025-05-03 22:13:59 +0900

    Add Solana wallet integration with Phantom wallet adapter and dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,24 @@ import OptionsPage from "pages/options";
 import { OptionsDataProvider } from "./providers/OptionsDataProvider";
 import { SnackbarProvider } from "components/SnackbarProvider";
 import { OptionTradesProvider } from "providers/OptionTradesProvider";
+import { SolanaWalletProvider } from "solana/WalletProvider";
 
 function App() {
   return (
-    <OptionsDataProvider>
-      <SnackbarProvider>
-        <OptionTradesProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<OptionsPage />} />
-              <Route path="/profile" element={<HistoryPage />} />
-            </Routes>
-          </BrowserRouter>
-        </OptionTradesProvider>
-      </SnackbarProvider>
-    </OptionsDataProvider>
+    <SolanaWalletProvider>
+      <OptionsDataProvider>
+        <SnackbarProvider>
+          <OptionTradesProvider>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<OptionsPage />} />
+                <Route path="/profile" element={<HistoryPage />} />
+              </Routes>
+            </BrowserRouter>
+          </OptionTradesProvider>
+        </SnackbarProvider>
+      </OptionsDataProvider>
+    </SolanaWalletProvider>
   );
 }
 
